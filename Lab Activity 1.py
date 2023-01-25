55e54879d571c56af5c4e43bf9d68eab8b607bd3
Vincenza Paradiso <122514035+vinniparadiso@users.noreply.github.com>
2023-01-25 15:57:07 -0500

    Add files via upload

---
+++ Lab Activity 1.py
@@ -0,0 +1,38 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#The authors' names are: Vincenza Paradiso and Emily Nixon
+1+2*3
+7
+(1+2)*3
+9
+7/3
+2.3333333333333335
+7//3
+2
+7%3
+1
+22/4
+5.5
+22//4
+5
+22%4
+2
+3**2
+9
+2**3
+8
+6%2
+0
+(4+3)//2
+3
+1+2*(3**4)
+163
+print("2+2", 2+2)
+2+2 4
+print("2+2=", 2+2)
+2+2= 4
+print("50 pounds is", 50*0.453592, "Kilograms").
+SyntaxError: invalid syntax
+
+print("50 pounds is", 50*0.453592, "Kilograms")
+50 pounds is 22.6796 Kilograms
